d8339a226503b2692adf71a9a9ba6cab8d758bfd
kartic <karticchaudhary@gmail.com>
2024-05-28 01:33:42 +0530

    first commit

---
+++ catalog-service/src/test/java/com/project/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.project.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import(ContainersConfig.class)
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ catalog-service/src/test/java/com/project/catalog_service/TestCatalogServiceApplication.java
@@ -0,0 +1,15 @@
+package com.project.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication
+                .from(CatalogServiceApplication::main)
+                .with(ContainersConfig.class)
+                .run(args);
+    }
+
+}
